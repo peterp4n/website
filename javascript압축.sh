d509cde91559c59e0700cb6dd07e757b961b2728
peterp4n <peterp4n@gmail.com>
2018-11-30 09:35:03 +0900

    Update javascript압축.sh

--- javascript압축.sh
+++ javascript압축.sh
@@ -1,3 +1,21 @@
+##### jshint로분석하기
+1. jshint 설치
+npm install jshint -g 
+
+2. jshint 실행
+jshint 파일명.js 
+
+##### eslint로분석하기
+1. eslint 설치 
+npm install -g eslint
+
+2. eslint 실행 
+eslint 파일명.js
+
+
+
+
+#### 압축하기
 1. 설치
 npm install uglify-js -g
 
