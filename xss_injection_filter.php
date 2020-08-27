0036caff9d0a2210daf7441b28481639a6d1e483
peterp4n <peterp4n@gmail.com>
2020-08-27 11:49:44 +0900

    Create xss_injection_filter.php

---
+++ xss_injection_filter.php
@@ -0,0 +1,4 @@
+1. filter_input - https://www.php.net/manual/en/function.filter-input.php
+2. pdo bind - https://www.php.net/manual/en/ref.pdo-mysql.php
+3. HTML Purifier - http://htmlpurifier.org/
+4. web firewall
