ea094b26c169b2f93634deca696e8031f2f01ba0
peterp4n <peterp4n@gmail.com>
2020-08-27 11:52:12 +0900

    Update xss_injection_filter.php

--- xss_injection_filter.php
+++ xss_injection_filter.php
@@ -1,4 +1,5 @@
 1. filter_input - https://www.php.net/manual/en/function.filter-input.php
 2. pdo bind - https://www.php.net/manual/en/ref.pdo-mysql.php
 3. HTML Purifier - http://htmlpurifier.org/
-4. web firewall
+4. csrf - https://stackoverflow.com/questions/6287903/how-to-properly-add-cross-site-request-forgery-csrf-token-using-php
+5. web firewall
